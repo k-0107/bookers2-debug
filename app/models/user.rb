0137d7bc9c64d39b72b301e1e5e02cc77788bc15
EC2 Default User <ec2-user@ip-172-31-47-110.ap-northeast-1.compute.internal>
2024-01-14 08:04:15 +0000

    [Finish]検索結果一覧まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
@@ -31,7 +31,20 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
-  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE ?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE ?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE ?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE ?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
