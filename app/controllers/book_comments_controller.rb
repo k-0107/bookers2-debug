aa0e32caf12edf50e210d53c25a24a56b18d2ccd
EC2 Default User <ec2-user@ip-172-31-47-110.ap-northeast-1.compute.internal>
2024-01-15 07:32:54 +0000

    [Finish]コメント機能の非同期処理

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,17 +2,17 @@ class BookCommentsController < ApplicationController
   
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
     # comment = current_user.post_comments.new(post_comment_params) <---5.6行目の省略形
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+    @comment.book_id = book.id
+    @comment.save
   end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
   
   private
