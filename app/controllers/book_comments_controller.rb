48f15965b0b854ceb7edf81230acf62ea1fa448b
EC2 Default User <ec2-user@ip-172-31-47-110.ap-northeast-1.compute.internal>
2024-01-15 07:43:43 +0000

    [Finish]コメント機能の非同期処理

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,7 +10,6 @@ class BookCommentsController < ApplicationController
   end
   
   def destroy
-    book = Book.find(params[:book_id])
     @comment = BookComment.find(params[:id])
     @comment.destroy
   end
