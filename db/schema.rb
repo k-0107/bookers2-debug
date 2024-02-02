62dc09e25ff45f61f5eb8bb09367317fe11a79c5
k-0107 <k.tkm0107@gmail.com>
2024-02-02 09:21:17 +0000

    [Finish]7d:Raty実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_19_225319) do
+ActiveRecord::Schema.define(version: 2024_02_02_054736) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2024_01_19_225319) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "star"
   end
 
   create_table "favorites", force: :cascade do |t|
